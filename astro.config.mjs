e25105a997b853c00ea2a72f220b6cabebc29d5b
Brian Duong <dbiny3css@gmail.com>
2026-05-26 14:38:58 -0500

    chores: Configure deploy to gh pages action
    Just followed the instructions according to the astro docs:
    https://docs.astro.build/en/guides/deploy/github/#how-to-deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://BDuong0.github.io',
+  base: '/my-repo',
   integrations: [
     starlight({
       title: "compose-layouts",
